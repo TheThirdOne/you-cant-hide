b5a86da3499b114a7e0526d05c8b5b3dc4c6dd0c
Benjamin Landers <benjaminrlanders@gmail.com>
2013-08-24 20:00:47 -0700

    added sound code

--- javascript/main.js
+++ javascript/main.js
@@ -127,3 +127,31 @@ function start(){
     window.setInterval(loop,constants.playloop);
   }
 }
+
+var sounds = {};
+init_sound('hurt',5, .5);
+function init_sound(type, channels, volume){
+  sounds[type]=[]
+  for (a=0;a<channels;a++) {                  
+    sounds[type][a] = {};
+    sounds[type][a]['channel'] = new Audio();   
+    sounds[type][a]['channel'].src = document.getElementById(type).src; 
+    sounds[type][a]['channel'].load();      
+    sounds[type][a]['finished'] = -1;
+    if(volume)
+      sounds[type][a]['channel'].volume = volume;         
+  }
+}
+
+function play_multi_sound(s, start) {
+  for (a=0;a<sounds[s].length;a++) {
+    thistime = new Date();
+    temp = sounds[s]
+    if (sounds[s][a]['finished'] < thistime.getTime()) {      
+      sounds[s][a]['finished'] = thistime.getTime() + document.getElementById(s).duration*1000 + start*1000;
+      sounds[s][a]['channel'].currentTime = start;
+      sounds[s][a]['channel'].play();
+      break;
+    }
+  }
+}
